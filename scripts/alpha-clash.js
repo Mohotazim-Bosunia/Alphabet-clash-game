bf256c42143f9db264a6ffcd5a70fa93214eefe4
Mohotazim-Bosunia <zimhossen0@gmail.com>
2024-11-15 01:07:30 +0600

    fourth commit

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,7 +1,11 @@
+/*
 function play(){
     const homeSection=document.getElementById('home-screen')
     console.log(homeSection.classList.add('hidden'));
 
     const playgroundSection=document.getElementById('play-ground');
     console.log(playgroundSection.classList.remove('hidden'))
+}*/ 
+function play(){
+    hideElementById('home-screen')
 }
