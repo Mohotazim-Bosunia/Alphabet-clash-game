dccb76a5e1fc93796f4457efacede0817f38c0dc
Mohotazim-Bosunia <zimhossen0@gmail.com>
2024-11-15 18:34:48 +0600

    fourth commit

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -6,6 +6,45 @@ function play(){
     const playgroundSection=document.getElementById('play-ground');
     console.log(playgroundSection.classList.remove('hidden'))
 }*/ 
+
+//capture keybord key press
+document.addEventListener('keyup',function handleKeyButtonPress(event){
+    const playerPressed=event.key;
+    //console.log('Player pressed',event.key)
+
+    const currentAlphabetElement=document.getElementById("currentAlphabet");
+    const currentAlphabet=currentAlphabetElement.innerText;
+    const expectedAlphabet=currentAlphabet.toLowerCase()
+    console.log(playerPressed,expectedAlphabet)
+
+    //check matched or not
+    if(playerPressed==expectedAlphabet){
+        console.log('you get a point')
+        removeBackgroundColorById(expectedAlphabet)
+        continueGame()
+    }
+    else{
+        console.log('you missed.you lost a life') 
+    }
+})
+    
+
+
+function continueGame(){
+    //step-1:generate a random alphabet
+    const alphabet=getARandomAlphabet()
+    console.log('Youe random Alphabet',alphabet)
+
+    //set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement=document.getElementById("currentAlphabet");
+    currentAlphabetElement.innerText=alphabet
+
+    //set background color
+    setBackgroundColorById(alphabet)
+}
+
 function play(){
-    hideElementById('home-screen')
+    hideElementById("home-screen")
+    showElementById('play-ground')
+    continueGame()
 }
