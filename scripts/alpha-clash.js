cd8993d9563ecc7aa5e3b214430b3bca0eab2784
Mohotazim-Bosunia <zimhossen0@gmail.com>
2024-11-16 00:02:56 +0600

    fifth commit

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -19,13 +19,56 @@ document.addEventListener('keyup',function handleKeyButtonPress(event){
 
     //check matched or not
     if(playerPressed==expectedAlphabet){
-        console.log('you get a point')
+     
+      
+        //update score
+
+        /**      //1.get the current score
+        const currentScoreElement=document.getElementById('current-score')
+        const currentScoreText=currentScoreElement.innerText;
+        const currentScore=parseInt(currentScoreText);
+        console.log(currentScore)
+
+        //2.increase the score by 1
+        const newScore=currentScore + 1
+        
+        //3.show the update score
+        currentScoreElement.innerText=newScore; */
+        //console.log('you get a point')
+        
+        const currentScore=getTextElementValueById('current-score')
+        const updatedScore=currentScore+1;
+        setTextElementValueById('current-score',updatedScore)
+         
+        
+        //start a new round
         removeBackgroundColorById(expectedAlphabet)
         continueGame()
     }
     else{
         console.log('you missed.you lost a life') 
+      
+      /**   //step-1: get the current Life number
+          const currentLifeElement=document.getElementById('current-life')
+          const currentLifeText=currentLifeElement.innerText;
+          const currentLife=parseInt(currentLifeText);
+
+        //step-2:reduce the life count
+          const newLife=currentLife - 1;
+        //step-3:Disply the updated life count
+        currentLifeElement.innerText=newLife; */
+
+        const currentScore=getTextElementValueById('current-life')
+        const updatedScore=currentScore-1;
+        setTextElementValueById('current-life',updatedScore)
+
+        if(updatedScore==0){
+            gameOver()
+        }
     }
+
+    
+
 })
     
 
@@ -44,7 +87,22 @@ function continueGame(){
 }
 
 function play(){
+    //hide everything show only the playground
     hideElementById("home-screen")
+    hideElementById("final-score")
     showElementById('play-ground')
-    continueGame()
+    
+    //reset score and life
+    setTextElementValueById('current-life',5)
+    setTextElementValueById('current-score',0)
+}
+
+function gameOver(){
+    hideElementById("play-ground")
+    showElementById('final-score')
+
+    //show score
+    const lastScore=getTextElementValueById('current-score');
+    setTextElementValueById('last-score',lastScore)
+    
 }
